f28263557b262d0e9ff91d88c0babf20a47f65d1
Marcelo Miyachi <marcelo.m1811@gmail.com>
2022-06-07 15:19:33 -0300

    feat(app): add starships table

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,57 @@
 import React from "react";
-import logo from "./logo.svg";
+import { useStarships } from "./hooks";
+import { Starship } from "./types/starship";
+
+interface StarshipTableColumn {
+  name: keyof Starship;
+  label: string;
+}
 
 function App() {
+  const { starships, error, loading } = useStarships();
+  const columns: StarshipTableColumn[] = [
+    {
+      name: "name",
+      label: "Name",
+    },
+    {
+      name: "model",
+      label: "Model",
+    },
+    {
+      name: "MGLT",
+      label: "MGLT",
+    },
+    {
+      name: "cargo_capacity",
+      label: "Cargo Capacity",
+    },
+    {
+      name: "consumables",
+      label: "Consumables",
+    },
+  ];
+
   return (
     <div>
-      <h1>oi</h1>
+      <table>
+        <thead>
+          <tr>
+            {columns.map(({ name, label }) => (
+              <th key={name}>{label}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {starships.map((starship) => (
+            <tr key={starship.url}>
+              {columns.map(({ name }) => (
+                <td key={name}>{starship[name]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
